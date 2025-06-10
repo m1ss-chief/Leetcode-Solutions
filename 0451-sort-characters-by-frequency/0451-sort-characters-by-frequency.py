1788d083cf4700349c3a80e2edad0c14a6589d5d
Mili Parashar <96782801+m1ss-chief@users.noreply.github.com>
2025-06-10 16:18:47 +0530

    Time: 36 ms (9.77%), Space: 19.4 MB (25.37%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -8,7 +8,7 @@ class Solution:
             else:
                 res[s[i]] += 1
         res = dict(sorted(res.items(),key=lambda item: item[1],reverse=True))
-        print (res)
+
         for x in res:
             while res[x] != 0:
                 ans += x
