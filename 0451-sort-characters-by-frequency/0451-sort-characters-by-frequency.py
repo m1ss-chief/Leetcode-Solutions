44717cbcf737242a9b045ba6de01826cb44543b1
Mili Parashar <96782801+m1ss-chief@users.noreply.github.com>
2025-06-10 16:17:29 +0530

    Time: 42 ms (7.54%), Space: 19.5 MB (23.91%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,20 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        s = list(s)
+        s.sort()
+        res = {}
+        ans = ""
+        for i in range(len(s)):
+            if s[i] not in res:
+                res[s[i]] = 1
+            else:
+                res[s[i]] += 1
+        res = dict(sorted(res.items(),key=lambda item: item[1],reverse=True))
+        print (res)
+        for x in res:
+            while res[x] != 0:
+                ans += x
+                res[x] -= 1
+
+        return ans
+        
