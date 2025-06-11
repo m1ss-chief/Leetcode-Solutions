314a281497dd7aa1c5127320db428de3bca0a269
Mili Parashar <96782801+m1ss-chief@users.noreply.github.com>
2025-06-11 12:34:52 +0530

    Time: 7813 ms (8.57%), Space: 17.9 MB (60.57%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -12,6 +12,28 @@ class Solution:
                     max_s = sub_s
             sub_s = ""
         return max_s if max_s != "" else s[0]
+
+        # n = len(s)
+        # max_s = ""
+        # if s == s[::-1]:
+        #     return s
+        # for i in range(1,n):
+        #     sub_s = s[i:]
+        #     if sub_s == sub_s[::-1] and len(max_s) < len(sub_s):
+        #         max_s = sub_s
+        #         sub_s = ""
+        # for i in range(1,n):
+        #     sub_s = s[:-i]
+        #     if sub_s == sub_s[::-1] and len(max_s) < len(sub_s):
+        #         max_s = sub_s
+        #         sub_s = ""
+        # for i in range(1,n):
+        #     sub_s = s[i:-i]
+        #     if sub_s == sub_s[::-1] and len(max_s) < len(sub_s):
+        #         max_s = sub_s
+        #         sub_s = ""
+        # return max_s if max_s != "" else s[0]
+            
                 
                 
 
