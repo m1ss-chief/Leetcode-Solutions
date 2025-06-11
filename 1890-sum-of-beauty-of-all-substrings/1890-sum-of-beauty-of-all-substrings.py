2537f6ae86f6d0eb45e45d2260634a4d3ea5e925
Mili Parashar <96782801+m1ss-chief@users.noreply.github.com>
2025-06-11 14:06:22 +0530

    Time: 2335 ms (50.77%), Space: 17.6 MB (94.85%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.py
@@ -1,25 +1,34 @@
 class Solution:
     def beautySum(self, s: str) -> int:
-        def beauty(word):
-            freq = {}
-            for i in range(len(word)):
-                if word[i] not in freq:
-                    freq[word[i]] = 1
-                else:
-                    freq[word[i]] += 1
-            beauty = max(freq.values()) - min(freq.values())
+        # def beauty(word):
+        #     freq = {}
+        #     for i in range(len(word)):
+        #         if word[i] not in freq:
+        #             freq[word[i]] = 1
+        #         else:
+        #             freq[word[i]] += 1
+        #     beauty = max(freq.values()) - min(freq.values())
             
-            return beauty
-        sum_beauty = 0
+        #     return beauty
+        # sum_beauty = 0
+        # for i in range(len(s)):
+        #     word = s[i]
+        #     for j in range(i+1,len(s)):
+        #         word += s[j]
+                
+        #         b = beauty(word)
+                
+        #         if b!= 0:
+        #             sum_beauty += b
+        # return sum_beauty
+
+    
+        ans = 0 
         for i in range(len(s)):
-            word = s[i]
-            for j in range(i+1,len(s)):
-                word += s[j]
-                
-                b = beauty(word)
-                
-                if b!= 0:
-                    sum_beauty += b
-        return sum_beauty
+            freq = [0]*26
+            for j in range(i, len(s)):
+                freq[ord(s[j])-97] += 1
+                ans += max(freq) - min(x for x in freq if x)
+        return ans
 
         
